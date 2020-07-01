cc57d93e500f176f002e7a16b614857d2569c2d9
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-01 14:36:57 +0300

    init

--- LazyVGridItem/ContentView.swift
+++ LazyVGridItem/ContentView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var minSpacing: CGFloat = 30
+    @State private var maxSpacing: CGFloat = 50
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack(spacing: 20) {
+            let gridItem = [GridItem(.adaptive(minimum: minSpacing, maximum: maxSpacing))]
+            
+            Text("LazyVGrid")
+            
+            LazyVGrid(columns: gridItem, spacing: 20) {
+                ForEach(0..<40) { index in
+                    Image(systemName: "\(index).circle")
+                        .frame(width: 20, height: 20)
+                }
+                Image(systemName: "arrow.right.circle")
+            }
+            .frame(maxHeight: .infinity)
+            .border(Color.blue)
+            .padding(.all, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            
+            VStack {
+                Slider(value: $minSpacing, in: 10...40, step: 5, minimumValueLabel: Text("10"), maximumValueLabel: Text("40")) {
+                    Text("Minimum Spacing")
+                }
+                Slider(value: $maxSpacing, in: 20...50, step: 5, minimumValueLabel: Text("20"), maximumValueLabel: Text("50")) {
+                    Text("Minimum Spacing")
+                }
+            }.padding(.all, 10)
+            
+        }
+        .animation(.default)
+        .font(.title)
     }
 }
 
