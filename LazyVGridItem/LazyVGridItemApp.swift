134160ff742c278951e947dc364c685f336c49fa
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-01 14:25:11 +0300

    Initial Commit

---
+++ LazyVGridItem/LazyVGridItemApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyVGridItemApp.swift
+//  LazyVGridItem
+//
+//  Created by ramil on 01.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct LazyVGridItemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
